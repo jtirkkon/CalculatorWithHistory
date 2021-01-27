b7d165bc2d554cb5be4a8463495fd6990527e120
jtirkkon <jouni.tirkkonen@gmail.com>
2021-01-27 10:36:17 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+//<FlatList data={calculations} renderItem={({item}) => <Text>{item.key}</Text>} />
 
 export default function App() {
+  const [numberOne, setNumberOne] = useState('');
+  const [numberTwo, setNumberTwo] = useState('');
+  const [result, setResult] = useState(0);
+  const [calculations, setCalculations] = useState([]);
+
+  const addition = () => {
+    const sum = parseFloat(numberOne) + parseFloat(numberTwo);
+    setResult(sum);
+    setCalculations([...calculations, {key: `${numberOne} + ${numberTwo} = ${sum}`}]);
+    setNumberOne('');
+    setNumberTwo('');
+  }
+
+  const subtraction = () => {
+    const difference = parseFloat(numberOne) - parseFloat(numberTwo);
+    setResult(difference);
+    setCalculations([...calculations, {key: `${numberOne} - ${numberTwo} = ${difference}`}]);
+    setNumberOne('');
+    setNumberTwo('');  
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: {result}</Text>
+      <TextInput style={styles.textInputStyle} onChangeText={text => setNumberOne(text)} value={numberOne} keyboardType="number-pad"></TextInput>
+      <TextInput style={styles.textInputStyle} onChangeText={text => setNumberTwo(text)} value={numberTwo} keyboardType="number-pad"></TextInput>
+      
+      <View style={styles.buttonContainer}>
+        <View style={{marginRight: 30}}>
+          <Button title="+" onPress={addition}></Button>
+        </View>
+        <Button title="-" onPress={subtraction}></Button>
+      </View>
+
+      <Text>History</Text>
+      <FlatList data={calculations} renderItem={({item}) => <Text>{item.key}</Text>} />
+      
+      
+     
     </View>
   );
 }
@@ -16,6 +52,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100
   },
+  textInputStyle: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginBottom: 30
+  }
 });
